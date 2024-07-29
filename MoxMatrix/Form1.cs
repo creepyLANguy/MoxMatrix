7cab7a3cd0c151e3178bff9ae78dbc8bf2599876
Altamish Mahomed <gambit318@gmail.com>
2024-07-29 22:56:28 +0200

    Removed invalid store defn.

--- MoxMatrix/Form1.cs
+++ MoxMatrix/Form1.cs
@@ -39,7 +39,6 @@ namespace MoxMatrix
       {2, ""},
       {3, ""},
       {4, ""},
-      {6, ""},
       {11, ""},
       {13, ""},
       {15, ""},
@@ -51,6 +50,8 @@ namespace MoxMatrix
       {34, ""},
       {41, ""},
       {44, ""}
+
+https://moxmonolith.com/card/7597/products?retailers[]=2&retailers[]=3&retailers[]=4&retailers[]=11&retailers[]=13&retailers[]=15&retailers[]=18&retailers[]=19&retailers[]=20&retailers[]=21&retailers[]=26&retailers[]=34&retailers[]=41&retailers[]=44&poll=0
     };
 
     private readonly string _outputFolder = "queries";
